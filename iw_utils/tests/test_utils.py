8d19e1d07918a691307969727e779b8abe2bd956
Ife walter <ifewalte@gmail.com>
2018-06-22 23:16:04 +0100

    circleCI

--- iw_utils/tests/test_utils.py
+++ iw_utils/tests/test_utils.py
@@ -1,7 +1,16 @@
 from unittest import TestCase
 
-import iw_utils
+from iw_utils.utils import Util
+
 
 class UtilsTest(TestCase):
-    def test_is_string(self):
-        self.assertTrue(True)
+    def test_encrypted_string(self):
+        plaint_text = 'test'
+        encrypted_string = Util.encrypt3DESData('testkey',plaint_text)
+        print(encrypted_string)
+        self.assertNotEqual(plaint_text, encrypted_string)
+
+    def test_decrypted_string(self):
+        encrypted_text = 'cinQDDzrFSk='
+        decrypted_text = Util.decrypt3DESData('testkey',encrypted_text)
+        self.assertNotEqual(decrypted_text, encrypted_text)
